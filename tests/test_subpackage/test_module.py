ef219218c4874c3ede563843414d5b650b4b91fa
aymanelsayeed <ayman.elsayeed@mdclone.com>
2024-08-21 12:04:56 +0300

    add docstring.

--- tests/test_subpackage/test_module.py
+++ tests/test_subpackage/test_module.py
@@ -1,4 +1,15 @@
+"""
+
+Tests for the module.py file in the subpackage folder.
+
+"""
+
+from pythonPackage.subpackage.module import subpackage_module_add_function
 
 
 def test_subpackage_module_add_function():
+    """
+    Test the subpackage_module_add_function function.
+    :return: None
+    """
     assert subpackage_module_add_function(1, 2) == 3
