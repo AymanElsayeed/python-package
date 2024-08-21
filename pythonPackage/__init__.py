51d8c4ffaa47455b781b87d08a86e15d6715a898
aymanelsayeed <ayman.elsayeed@mdclone.com>
2024-08-21 12:59:37 +0300

    update version.

--- pythonPackage/__init__.py
+++ pythonPackage/__init__.py
@@ -4,4 +4,6 @@ Main init file for the package of this project.
 
 """
 
+__version__ = "0.1.0"
+
 from . import subpackage
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='my_package',
-    version='0.1',
+    version='0.1.0',
     author='Ayman Elsayeed',
     description='A small example package',
     packages=find_packages(include=['python-package', 'python-package.*']),
