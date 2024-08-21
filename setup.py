f85429dea1e3c4fd5285d5997007573037307325
aymanelsayeed <ayman.elsayeed@mdclone.com>
2024-08-21 13:27:57 +0300

    add classifiers.

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
+
 from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    history = history_file.read()
 
 setup(
     name='my_package',
@@ -8,4 +15,12 @@ setup(
     packages=find_packages(include=['python-package', 'python-package.*']),
     install_requires=['numpy', 'pandas', 'scikit-learn'],
     python_requires='>=3.6',
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Development Status :: 1 - pre-alpha',
+        'Intended Audience :: Developers',
+    ],
 )
